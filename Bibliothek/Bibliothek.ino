d3c33dce0c8dcd261b22d02b0de2e57ee012e454
Harald Moritz <harald.moritz@htl.rennweg.at>
2018-01-24 20:47:27 +0100

    Add Arduino I2C Code

---
+++ Bibliothek/Bibliothek.ino
@@ -0,0 +1,257 @@
+#include <Wire.h>
+
+#include <FastLED.h>
+#include <Servo.h>
+#include <SparkFun_APDS9960.h>
+#include <SparkFunMLX90614.h>
+#include <SparkFunMPU9250-DMP.h>
+
+// SERIAL
+char serIn[100];
+char serOut[100];
+
+// ACTORS
+// FastLED
+#define NUM_LEDS 1
+#define DATA_PIN 6
+CRGB leds[NUM_LEDS];
+
+// Servos
+Servo thermoServo;
+Servo ejectionServo;
+
+// SENSORS
+// APDS
+SparkFun_APDS9960 apds = SparkFun_APDS9960();
+uint16_t colors[4];
+
+// Temp
+IRTherm therm;
+typedef struct {
+    double object;
+    double ambient;
+} temp_t;
+temp_t *t;
+
+// 10Dof
+MPU9250_DMP _10dof;
+float arr[3];
+char double_str0[13];
+char double_str1[13];
+char double_str2[13];
+unsigned char width = 12;
+unsigned char prec = 5;
+
+void setup() {
+  Serial.begin(9600);
+  setupFastLED();
+  setupServos();
+  //setupAPDS();
+  setupTemp();
+  setup10dof();
+}
+
+void loop() {
+  mainProg();
+}
+
+boolean equals(char* str1, char* str2) {
+    return strncmp(str1, str2, strlen(str2)) == 0;
+}
+
+void mainProg() {
+  memset(&serIn, 0, sizeof(serIn));
+  memset(&serOut, 0, sizeof(serOut));
+  memset(&double_str0, 0, sizeof(double_str0));
+  memset(&double_str1, 0, sizeof(double_str1));
+  memset(&double_str2, 0, sizeof(double_str2));
+  while (!Serial.available()) { }
+  read(serIn);
+  write(serIn);
+  if (equals(serIn, ">getTemp")) {
+        if (t = getTemperature()) {
+            sprintf(serOut, "<temp:%s", dtostrf(t->object, width, prec, double_str0));
+        } else {
+            sprintf(serOut, "<temp:null");
+        }
+    } else if (equals(serIn,  ">getAccel")) {
+        if (getAccel(arr)) {
+            sprintf(serOut, "<accel:%s,%s,%s",
+                    dtostrf(arr[0], width, prec, double_str0),
+                    dtostrf(arr[1], width, prec, double_str1),
+                    dtostrf(arr[2], width, prec, double_str2)
+            );
+        } else {
+            sprintf(serOut, "<accel:null");
+        }
+    } else if (equals(serIn, ">getGyro")) {
+        if (getGyro(arr)) {
+            sprintf(serOut, "<gyro:%s,%s,%s",
+                    dtostrf(arr[0], width, prec, double_str0),
+                    dtostrf(arr[1], width, prec, double_str1),
+                    dtostrf(arr[2], width, prec, double_str2)
+            );
+        } else {
+            sprintf(serOut, "<gyro:null");
+        }
+    } else if (equals(serIn, ">getMag")) {
+        if (getMag(arr)) {
+            sprintf(serOut, "<mag:%s,%s,%s",
+                    dtostrf(arr[0], width, prec, double_str0),
+                    dtostrf(arr[1], width, prec, double_str1),
+                    dtostrf(arr[2], width, prec, double_str2)
+            );
+        } else {
+            sprintf(serOut, "<mag:null");
+        }
+    } else if (equals(serIn, ">getColors")) {
+        if (getColors((uint16_t *)colors)) {
+            sprintf(serOut, "<colors:%i,%i,%i", colors[0], colors[1], colors[2]);
+        } else {
+            sprintf(serOut, "<colors:null");
+        }
+    } else if (equals(serIn, ">setColor:")) {
+        int num = 0;
+        sscanf(serIn, ">setColor:%i", &num);
+        displayColor(num);
+        sprintf(serOut, "<setColor:%i", num);
+    } else if (equals(serIn, ">setThermoServo:")) {
+        int num = 0;
+        sscanf(serIn, ">setThermoServo:%i", &num);
+        thermoServo.write(num);
+        sprintf(serOut, "<thermoServo:%i", num);
+    } else if (equals(serIn, ">setEjectSpeed:")) {
+        int num = 0;
+        sscanf(serIn, ">setEjectSpeed:%i", &num);
+        setSpeed(num);
+        sprintf(serOut, "<speed:%i", num);
+    } else {
+        sprintf(serOut, "<null");
+    }
+    write(serOut);
+}
+
+void read(char* buff) {
+  int index = 0;
+    while(Serial.available()) {
+        buff[index] = Serial.read();
+        index ++;
+        delay(10);
+    }
+}
+
+void write(char* buff) {
+  Serial.println(buff);
+}
+
+void setupFastLED() {
+  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+}
+
+void displayColor(uint16_t color) {
+  leds[0] = CHSV(color, 255, 255);
+  FastLED.show();
+}
+
+void setupServos() {
+  thermoServo.attach(3);
+  ejectionServo.attach(2);
+}
+
+void setSpeed(int percent) {
+  ejectionServo.write(map(percent, 0, 100, 0, 180));
+}
+
+void setupAPDS() {
+  if ( !apds.init() ) {
+    Serial.println("Something went wrong during APDS-9960 init!");
+  }  
+  if ( !apds.enableLightSensor(false) ) {
+    Serial.println("Something went wrong during light sensor init!");
+  }
+}
+
+uint8_t getColors(uint16_t *arr) {
+    uint16_t ambient_light = 0;
+    uint16_t red_light = 0;
+    uint16_t green_light = 0;
+    uint16_t blue_light = 0;
+
+    if (  !apds.readAmbientLight(ambient_light) ||
+          !apds.readRedLight(red_light) ||
+          !apds.readGreenLight(green_light) ||
+          !apds.readBlueLight(blue_light) ) {
+        return 0;
+    }
+
+    arr[0] = ambient_light;
+    arr[1] = red_light;
+    arr[2] = green_light;
+    arr[3] = blue_light;
+
+    return 1;
+}
+
+void setupTemp() {
+  therm.begin();
+  therm.setUnit(TEMP_C);
+}
+
+temp_t *getTemperature() {
+    temp_t temp;
+    if (therm.read()) {
+        temp.object = therm.object();
+        temp.ambient = therm.ambient();
+        Serial.println(temp.object);
+        //Serial.println(temp.ambient);
+        return &temp;
+    } else {
+      Serial.println("Not working");
+        return NULL;
+    }
+}
+
+void setup10dof() {
+  if (_10dof.begin() != INV_SUCCESS)
+  {
+    while (1)
+    {
+      Serial.println("Unable to communicate with MPU-9250");
+      Serial.println("Check connections, and try again.");
+      Serial.println();
+      delay(5000);
+    }
+  }
+
+  _10dof.setSensors(INV_XYZ_GYRO); // Enable gyroscope only
+  _10dof.setGyroFSR(2000); // Set gyro to 2000 dps
+
+ /* _10dof.dmpBegin(DMP_FEATURE_GYRO_CAL |
+              DMP_FEATURE_SEND_CAL_GYRO,
+              10);*/
+}
+
+uint8_t getAccel(float *arr) {
+    uint8_t err = _10dof.updateAccel();
+    arr[0] = _10dof.calcAccel(_10dof.ax);
+    arr[1] = _10dof.calcAccel(_10dof.ay);
+    arr[2] = _10dof.calcAccel(_10dof.az);
+    return err;
+}
+
+uint8_t getGyro(float *arr) {
+    uint8_t err = _10dof.updateGyro();
+    arr[0] = _10dof.calcGyro(_10dof.gx);
+    arr[1] = _10dof.calcGyro(_10dof.gy);
+    arr[2] = _10dof.calcGyro(_10dof.gz);
+    return err;
+}
+
+uint8_t getMag(float *arr) {
+    uint8_t err = _10dof.updateCompass();
+    arr[0] = _10dof.calcMag(_10dof.mx);
+    arr[1] = _10dof.calcMag(_10dof.my);
+    arr[2] = _10dof.calcMag(_10dof.mz);
+    return err;
+}
+
