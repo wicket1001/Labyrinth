76703332f7e347d73308a74d3b3560801f3a5ae3
Harald Moritz <harald.moritz@htl.rennweg.at>
2018-01-26 16:47:48 +0100

    Serial Test

--- Raspberry/Serial.py
+++ Raspberry/Serial.py
@@ -1,10 +1,36 @@
 import serial
 import time
 
-s = serial.Serial('/dev/ttyAMA0', 9600)  # Namen ggf. anpassen
-s.open()
-time.sleep(5)  # der Arduino resettet nach einer Seriellen Verbindung, daher muss kurz gewartet werden
+#s = serial.Serial('/dev/ttyAMA0', 9600)  # Namen ggf. anpassen
+#s.open()
+#time.sleep(5)  # der Arduino resettet nach einer Seriellen Verbindung, daher muss kurz gewartet werden
 
+def getPorts():
+    arduinos = []
+    import serial.tools.list_ports_linux as listPorts
+    ports = listPorts.comports()
+
+    for p in ports:
+        if p[2] != 'n/a':
+            found = False
+            ser = serial.Serial(p[0])
+            time.sleep(2)
+            ser.write("name")
+            for i in range(1000):
+                time.sleep(0.01)
+                if ser.inWaiting():
+                    # print "inWaiting"
+                    numBytes = ser.inWaiting()
+                    inputBytes = ser.read(numBytes)
+                    found = True
+                    break
+            if found:
+                arduinos.append(serial.Serial(p[0]))
+
+    print(arduinos)
+
+getPorts()
+'''
 s.write(">setColor:1")
 try:
     while True:
@@ -12,4 +38,4 @@ try:
         print(response)
 except KeyboardInterrupt:
     s.close()
-
+'''
