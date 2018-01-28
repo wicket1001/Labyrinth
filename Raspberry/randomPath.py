5a3b7848081ab5ca0f7469ec694bfd0040f14fd5
wicket1001 <wicket1001@github.com>
2018-01-28 15:30:45 +0000

    Added randomPath

--- Raspberry/randomPath.py
+++ Raspberry/randomPath.py
@@ -1,52 +1,80 @@
 from ActorControl import *
 import time
+import random
 
 actorControl = ActorControl()
 motorControl = actorControl.getMotor()
 
 def rotate(direction):
-	if direction == 1:
-		motorControl.set([1, 0], 0.9, 50)
-	elif direction == -1:
-		motorControl.set([0, 1], 0.9, 50)
+	#direction *= -1
+	if direction > 0: # links
+		for i in range(abs(direction)):
+			motorControl.set([1, 0], 0.9, 50)
+	elif direction < 0: # rechts
+		for i in range(abs(direction)):
+			motorControl.set([0, 1], 0.8, 50)
 
 def drive(direction):
-	if direction == 1:
-		motorControl.set([1, 1], 1, 100)
-	elif direction == -1:
-		motorControl.set([0, 0], 1, 100)
+	#direction *= -1
+	if direction > 0:
+		for i in range(abs(direction)):
+			motorControl.set([1, 1], 1.6, 100)
+	elif direction < 0:
+		for i in range(abs(direction)):
+			motorControl.set([0, 0], 0.7, 100)
 
-for i in range(20):
-	drive(1)
-	drive(1)
-	rotate(1)
+def e():
+	while True:
+		mode = int(random.random() * 2)
+		drive(1)
+		if mode == 0:
+			rotate(-1)
+		else:
+			rotate(1)
+#		drive(-1)
+
+def block(speed, rotation):
+	drive(speed)
 	drive(-1)
-	drive(1)
-	drive(1)
-	rotate(1)
-	drive(-1)
-	drive(1)
-	drive(1)
-	rotate(-1)
-	drive(-1)
-#print "driving forward"
-#	motorControl.set([1,1],2.7,100) # forward
-#	motorControl.set([1,0],2.5,100) # left
-#	motorControl.set([0,0],2.5,100) # backward
-'''print "driving backward"
-motorControl.set([0,0],0.7,100)
-print "rotate"
-motorControl.set([1,0],0.9,50)
-print "forward"
-motorControl.set([1,1],1.5,100)
-print "rotate"
-motorControl.set([1,0],0.9,50)
-print "forward"
-motorControl.set([1,1],2,100)
-print "rotate"
-motorControl.set([0,1],0.9,50)
-print "forward"
-motorControl.set([1,1],1,100)
-print "backward"
-motorControl.set([0,0],1,100)
-'''
+	rotate(rotation)
+
+def f():
+	block(2, 1)
+	block(2, 1)
+	block(2, -1)
+	block(2, 1)
+	block(2, -1)
+	block(7, -1)
+
+def d():
+	while True:
+		for i in range(5):
+			for j in range(2):
+				block(2, 1)
+			block(2, -1)
+			block(2, 1)
+		block(2, -1)
+
+def c():
+	for i in range(10):
+		drive(1)
+		drive(-1)
+
+def b():
+	for i in range(10):
+		rotate(1)
+		rotate(-1)
+
+def a():
+	for i in range(50):
+		drive(3)
+		drive(-1)
+		rotate(1)
+		drive(3)
+		drive(-1)
+		rotate(1)
+		drive(3)
+		drive(-1)
+		rotate(-1)
+
+f()
